88dc9360b451534b5340f0b505a7eaccced85a06
Reese <reeseknott@gmail.com>
2023-04-24 12:15:08 -0700

    update

--- text.js
+++ text.js
@@ -44,3 +44,75 @@ const getBookFromInput = () => {
   }
   
   addBtn.addEventListener("click", createBookCard())
+
+
+
+
+
+
+  const btn = document.querySelector('.bookBtn');
+  const nav = document.querySelector('nav');
+  const addBtn = document.querySelector('.addBtn');
+  const list = document.querySelector('.list');
+  const title = document.querySelector('.title');
+  const author = document.querySelector('.author');
+  const pages = document.querySelector('.pages')
+  const read = document.querySelector('#page');
+  const form = document.querySelector('.form');
+
+btn.addEventListener('click', () => {
+  const form = document.querySelector('.form');
+
+  if (form.style.display === 'none') {
+
+    form.style.display = 'grid';
+    btn.style.display = 'none';
+   
+    nav.style.filter = 'blur(3px)';
+  } else {
+    form.style.display = 'none';
+
+  }
+});
+
+addBtn.addEventListener('click', () => {
+  
+});
+document.querySelector("body > div.container > form > button")
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+   const bookCard = document.createElement('div')
+   const newlabel = document.createElement("Label");
+   const buttonGroup = document.createElement('div')
+   const title = document.createElement('p')
+   const author = document.createElement('p')
+   const pages = document.createElement('p')
+  const readBtn = document.createElement('button')
+  const removeBtn = document.createElement('button')
+  title.textContent = `"${book.title}"`
+  author.textContent = book.author
+  pages.textContent = `${book.pages} pages`
+  removeBtn.textContent = 'Remove'
+  if (book.isRead) {
+    readBtn.textContent = 'Read'
+    readBtn.classList.add('btn-light-green')
+  } else {
+    readBtn.textContent = 'Not read'
+    readBtn.classList.add('btn-light-red')
+  }
+
+  list.appendChild(title)
+  list.appendChild(author)
+  list.appendChild(pages)
+  list.appendChild(readBtn)
+  list.appendChild(removeBtn)
+  list.appendChild(buttonGroup)
+  list.appendChild(bookCard)
+
+});
+
+
+
+
+console.log(myLibrary)
